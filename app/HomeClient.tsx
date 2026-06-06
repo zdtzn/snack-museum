e26478e18348c6c032d2651c67e28944082a6163
snack-museum <snack@museum.local>
2026-06-06 17:27:54 +0800

    补全首页板块（Brands/PriceComparison/AboutUs/Stores/Cooperation/Testimonials）

--- app/HomeClient.tsx
+++ app/HomeClient.tsx
@@ -1,9 +1,15 @@
-"use client";
+﻿"use client";
 
 import { useState, useEffect } from "react";
 import { HeroBanner } from "@/components/home/HeroBanner";
 import { CategoryNav } from "@/components/home/CategoryNav";
 import { SnackCardList } from "@/components/home/SnackCardList";
+import { Brands } from "@/components/home/Brands";
+import { PriceComparison } from "@/components/home/PriceComparison";
+import { AboutUs } from "@/components/home/AboutUs";
+import { Stores } from "@/components/home/Stores";
+import { Cooperation } from "@/components/home/Cooperation";
+import { Testimonials } from "@/components/home/Testimonials";
 import { FeedZone } from "@/components/home/FeedZone";
 import { CustomerService } from "@/components/snack/CustomerService";
 import { Snack, SnackCategory } from "@/lib/snacks";
@@ -33,6 +39,12 @@ export function HomeClient({ snacks }: Props) {
         setActiveCategory(activeCategory === cat ? undefined : cat)
       } />
       <SnackCardList snacks={filteredSnacks} />
+      <Brands />
+      <PriceComparison />
+      <AboutUs />
+      <Stores />
+      <Cooperation />
+      <Testimonials />
       <FeedZone snackNames={snacks.map(s => s.name)} />
       {showCustomerService && <CustomerService onClose={() => setShowCustomerService(false)} />}
     </>
